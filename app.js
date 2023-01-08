f65d768125f91edc90e9005315c62d84d95a0de0
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-01-08 21:58:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+
+const app = express();
+
+const items = ["Code","Sleep","Repeat"];
+const workitems =[]
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    let day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    if(req.body.list === "Work"){
+        workitems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work",function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workitems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workitems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+
+
+
+
+
